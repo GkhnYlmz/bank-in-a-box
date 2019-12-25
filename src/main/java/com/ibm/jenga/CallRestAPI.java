d475b69493e436ce9b5e2a638471bfd4974fa9f8
gokhanyilmaz <gokhanyilmaz>
2019-12-25 11:28:51 +0300

    System Out

--- src/main/java/com/ibm/jenga/CallRestAPI.java
+++ src/main/java/com/ibm/jenga/CallRestAPI.java
@@ -50,7 +50,8 @@ public class CallRestAPI {
 		    System.out.println(POST_PARAMS);
 		    System.out.println("Value Coming from bank-in-a-box-cm:- " + System.getenv("SAY_HELLO_GOKHAN"));
 		    System.out.println("Value2  Coming from bank-in-a-box-cm:- " + System.getenv("MERHABA"));
-		    //KOCSISTEM
+		    //QnB
+		    System.out.println("QnB");
 
 		    //http://ace-rest-route-bank-in-a-box.yohanhabanero-f359424cb9067b462eab3a25b794045a-0001.eu-de.containers.appdomain.cloud/odmcommisionrules/v1/getCommission
 		    System.out.println("URL is added as env variable:" + " " +  System.getenv("ACE_URL"));
